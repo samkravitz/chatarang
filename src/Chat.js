a3f5f179b72b12a919800308d73d3a384b9550d8
Sam Kravitz <samkravitz99@gmail.com>
2018-06-12 14:47:48 -0400

    Added 'Message' and 'MessageList' components

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react'
 
+import ChatHeader from './ChatHeader'
+import MessageList from './MessageList'
+
 class Chat extends Component{
     render(){
         return(
             <div className= "chat">
-            Chat
+                <ChatHeader />
+                <MessageList />
             </div>
         )
     }
---
+++ src/MessageList.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import Message from './Message'
+
+const MessageList = () => {
+    const messages = [
+        {
+            id: 1,
+            userName: 'samkravitz',
+            body: 'Chatting up a storm yo'
+        },
+
+        {
+            id: 2,
+            userName: 'dstrus',
+            body: 'This guy is so annoying I hate my job',
+        }
+    ]
+
+    return(
+        <div className="MessageList">
+            {
+                messages.map(msg => (
+                <Message key= {msg.id} message={msg} />
+             ))
+            }
+        </div>
+    )
+}
+
+export default MessageList
